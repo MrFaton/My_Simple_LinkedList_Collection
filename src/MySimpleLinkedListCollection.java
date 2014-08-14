cfd07d54d8b04b535c11ae7c5a5dbbc34ddee07c
MrFaton <firefly90@inbox.ru>
2014-08-14 15:12:52 +0300

    update from work

--- src/MySimpleLinkedListCollection.java
+++ src/MySimpleLinkedListCollection.java
@@ -8,11 +8,16 @@ public class MySimpleLinkedListCollection {
         tail = new Node(1, tail);
         tail = new Node(2, tail);
         tail = new Node(3, tail);
+
         Node tailB;
         tailB = new Node(0, null);
         tailB = new Node(1, tailB);
         tailB = new Node(5, tailB);
         tailB = new Node(3, tailB);
+
+        Node tailC;
+        tailC = listCreator(5, 8, 9, 7, 6);
+
 //        System.out.println("Основной список: " + printToString(tail));
 //        System.out.println("Добавляем в голову итеративно: "+printToString(addToHeadIter(77, tail)));
 //        System.out.println("Добавляем в голову рекурсивно: " + printToString(addToHeadRec(99, tail)));
@@ -22,7 +27,11 @@ public class MySimpleLinkedListCollection {
 //        System.out.println("Удалить голову рекурсивно: " + printToString(removeHeadRec(tail)));
 //        System.out.println("Удалить удалить позицию итеративно: " + printToString(removePositionIter(0, tail)));
 //        System.out.println("Удалить удалить позицию рекурсивно: " + printToString(removePositionRec(2, tail)));
-        System.out.println("Хвост А: " + printToString(tail)+"\nХвост В: "+printToString(tailB)+"\nРезультат сравнения: "+isEqualRec(tail, tailB));
+//        System.out.println("Хвост А: " + printToString(tail)+"\nХвост В: "+printToString(tailB)+"\nРезультат сравнения: "+isEqualRec(tail, tailB));
+//        System.out.println("Длина списка "+printToString(tail)+" равна "+length(tail)+" единиц(ы)");
+        System.out.println("Сумма всех элементов списка " + printToString(tailC) + " равна " + summOfList(tailC));
+//        System.out.println("Максимальное значение среди всех элементов списка " + printToString(tail) + " равно " + maxItem(tail));
+//        System.out.println("Создание списка с помощью цикла " + printToString(tailC));
     }
 
     public static Node addToHeadIter(int value, Node tail) {
@@ -79,35 +88,47 @@ public class MySimpleLinkedListCollection {
     }
 
     public static Node removePositionIter(int position, Node tail) {
-        Node currentItemIter=tail;
-        if (position==0){
-            tail=tail.next;
+        Node currentItemIter = tail;
+        if (position == 0) {
+            tail = tail.next;
         } else {
             int i = 0;
-            while (i<position-1){
+            while (i < position - 1) {
                 i++;
-                currentItemIter=currentItemIter.next;
+                currentItemIter = currentItemIter.next;
             }
-            currentItemIter.next=currentItemIter.next.next;
+            currentItemIter.next = currentItemIter.next.next;
         }
         return tail;
     }
 
     public static Node removePositionRec(int position, Node tail) {
-        if(position==0){
-            tail=tail.next;
+        if (position == 0) {
+            tail = tail.next;
         } else {
             removePositionInnerRec(0, position, tail);
         }
         return tail;
     }
 
-    public static Boolean isEqualRec(Node tail, Node tailB){
+    public static Boolean isEqualRec(Node tail, Node tailB) {
         if (tail != null && tailB != null) {
-            return (tail.value==tailB.value) && isEqualRec(tail.next, tailB.next);
+            return (tail.value == tailB.value) && isEqualRec(tail.next, tailB.next);
         } else {
-            return tail==tailB;
+            return tail == tailB;
         }
+    }
+
+    public static int length(Node tail) {
+        return (tail == null) ? 0 : 1 + length(tail.next);
+    }
+
+    public static int summOfList(Node tail) {
+        return (tail == null) ? 0 : (tail.value + summOfList(tail.next));
+    }
+
+    public static int maxItem(Node tail) {
+        return (tail == null) ? 0 : Math.max(tail.value, maxItem(tail.next));
     }
 
 //    Внутренние методы...
@@ -134,12 +155,12 @@ public class MySimpleLinkedListCollection {
         return tailInner;
     }
 
-    public static Node removePositionInnerRec(int i, int positionInner, Node tailInner){
-        if(i<positionInner-1){
+    public static Node removePositionInnerRec(int i, int positionInner, Node tailInner) {
+        if (i < positionInner - 1) {
             i++;
             removePositionInnerRec(i, positionInner, tailInner.next);
         } else {
-            tailInner.next=tailInner.next.next;
+            tailInner.next = tailInner.next.next;
         }
         return tailInner;
     }
@@ -151,5 +172,13 @@ public class MySimpleLinkedListCollection {
             tail = tail.next;
         }
         return result + "*";
+    }
+
+    public static Node listCreator(int... values) {
+        Node tailToCreate = null;
+        for (int k = values.length - 1; k >= 0; k--) {
+            tailToCreate = new Node(values[k], tailToCreate);
+        }
+        return tailToCreate;
     }
 }
